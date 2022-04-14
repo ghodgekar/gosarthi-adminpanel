12a33b72a97a4456273cb5956a42b8c372dcfe5e
ghodgekar <103119421+ghodgekar@users.noreply.github.com>
2022-04-14 17:46:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,49 +5,14 @@ let express = require('express'),
     bodyParser = require('body-parser'),
     dbConfig = require('./db/database');
 
-// // Connecting mongoDB
-// mongoose.Promise = global.Promise;
-// mongoose.connect(dbConfig.db, {
-//     useNewUrlParser: true
-// }).then(() => {
-//         console.log('Database connected')
-//     },
-//     error => {
-//         console.log('Database could not be connected : ' + error)
-//     }
-// )
-// Setting up express
-const app = express();
-app.get('/', function(req, res){
-res.json({"tutorial" : "Build REST API with node.js"});
-});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cors());
-// Api root
-// const userRoute = require('./routes/student.route')
-// app.use('/endpoint', userRoute)
-// Create port
-const port = process.env.PORT || 8080;
-// Conectting port
-const server = app.listen(port, () => {
-    console.log('Port connected to: ' + port)
-})
-// Find 404 and hand over to error handler
-app.use((req, res, next) => {
-    next(createError(404));
-});
-// Index Route
-app.get('/', (req, res) => {
-    res.send('invaild endpoint');
-});
-// error handler
-app.use(function (err, req, res, next) {
-    console.error(err.message);
-    if (!err.statusCode) err.statusCode = 500;
-    res.status(err.statusCode).send(err.message);
-});
-// Static build location
-app.use(express.static(path.join(__dirname, 'dist')));
+// Connecting mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true
+}).then(() => {
+        console.log('Database connected')
+    },
+    error => {
+        console.log('Database could not be connected : ' + error)
+    }
+)
